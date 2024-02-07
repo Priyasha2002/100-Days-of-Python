83062db0736e67e3f87407afbfeed761860fdfaf
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2024-02-07 23:05:06 +0530

    Day 22

--- Turtle Crossing/main.py
+++ Turtle Crossing/main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-import random
+import random 
 
 screen = Screen()
 screen.bgcolor("black")
