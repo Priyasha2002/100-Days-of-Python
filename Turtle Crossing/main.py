2580e689c285acdd5ceb629f2ca4efd92508c66b
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2024-02-07 23:00:55 +0530

    Day 23

---
+++ Turtle Crossing/main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Turtle, Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+import random
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # Detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 22:
+            game_is_on = False
+            scoreboard.collide_car()
+
+    # Detect collision with walls
+
+    if player.ycor() > 280:
+        player.finish_race()
+        car_manager.increase_car_speed()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
