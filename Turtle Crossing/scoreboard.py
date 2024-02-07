9a4d0540fd60d0ffc64061ad0a69bd1b4e4bb37d
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2024-02-07 23:07:28 +0530

    Day 23

--- Turtle Crossing/scoreboard.py
+++ Turtle Crossing/scoreboard.py
@@ -2,7 +2,7 @@ FONT = ("Courier", 24, "normal")
 from turtle import Turtle
 
 class Scoreboard(Turtle):
-    def __init__(self):
+    def __init__(self): 
         super().__init__() 
         self.color("white")
         self.penup()
