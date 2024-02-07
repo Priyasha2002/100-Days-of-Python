c89380eadae99523a95b9507a6988909c3bcd054
Priyasha Ghosh <90889014+Priyasha2002@users.noreply.github.com>
2024-02-07 23:05:42 +0530

    Day 22

--- Turtle Crossing/scoreboard.py
+++ Turtle Crossing/scoreboard.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
     def __init__(self):
-        super().__init__()
+        super().__init__() 
         self.color("white")
         self.penup()
         self.hideturtle()
